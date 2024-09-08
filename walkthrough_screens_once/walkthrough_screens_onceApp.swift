2720ee00fc1cd0b5373f3f83650cd4bc278f4b79
rishabhkochar27 <rishabhkochar27@gmail.com>
2024-09-08 21:38:45 +0530

    Initial Commit

---
+++ walkthrough_screens_once/walkthrough_screens_onceApp.swift
@@ -0,0 +1,17 @@
+//
+//  walkthrough_screens_onceApp.swift
+//  walkthrough_screens_once
+//
+//  Created by nuclei on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct walkthrough_screens_onceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
