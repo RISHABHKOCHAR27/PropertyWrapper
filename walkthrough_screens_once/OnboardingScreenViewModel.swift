d25897034ea814ea774f0258f8377efd67e9dfeb
rishabhkochar27 <rishabhkochar27@gmail.com>
2024-09-17 00:23:29 +0530

    Property wrappers SwiftUI.

--- walkthrough_screens_once/OnboardingScreenViewModel.swift
+++ walkthrough_screens_once/OnboardingScreenViewModel.swift
@@ -10,4 +10,19 @@ import Foundation
 class OnboardingScreenViewModel: ObservableObject {
     @Published var currentPage: Int = 1
     var totalPage: Int = 3
+    // Use UserDefaults to store onboarding completion status
+    private let onboardingKey = "hasSeenOnboarding"
+    
+    init() {
+        // Check if the onboarding has already been completed
+        if UserDefaults.standard.bool(forKey: onboardingKey) {
+            // Set currentPage to bypass the onboarding screens
+            currentPage = totalPage + 1
+        }
+    }
+    
+    // Method to set onboarding as completed
+    func completeOnboarding() {
+        UserDefaults.standard.set(true, forKey: onboardingKey)
+    }
 }
